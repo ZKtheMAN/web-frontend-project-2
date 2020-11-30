c0e4180b81fe1457fd70297cb392bcf6f10ceb5c
ZKtheMAN <zakhattak@gmail.com>
2020-11-30 03:38:24 -0800

    All views can now be seen from the nav rail

--- src/GraphView.tsx
+++ src/GraphView.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {ResponsiveLine} from '@nivo/line';
 import {grabData} from './GrabDataHelpers';
+import Searchbar from './Searchbar';
 
 // taken direct from Nivo's website lmao
 interface GraphLine {
@@ -9,6 +10,11 @@ interface GraphLine {
         x: number | string | Date,
         y: number | string | Date
     }>
+}
+
+interface StateHistoryData {
+    date: number,
+    positive: number,
 }
 
 type GraphViewState = {
@@ -21,14 +27,66 @@ export class GraphView extends React.Component<{}, GraphViewState> {
 
     componentDidMount() {
         grabData("https://api.covidtracking.com/v1/states/ca/daily.json",
-                (val, index) => {
-                    
-                })
+            (val: StateHistoryData, index) => {
+                let dateAsString = val.date.toString();
+                let formString = dateAsString.substring(0, 4) + "-" + dateAsString.substring(4, 6) + "-" + dateAsString.substring(6);
+                return {
+                    x: formString,
+                    y: val.positive
+                }
+            }
+        ).then((x) => this.setState({
+            graphData: [{
+                id: 0,
+                data: x.reverse()
+            }]
+        }));
     }
 
     render() {
         return (
-            <p>bitch</p>
+            <div className="GraphView">
+                <div style={{
+                    display: 'flex',
+                    flexDirection: "row",
+                    justifyContent: 'flex-start',
+                    alignItems: 'center'
+                }}>
+                    <Searchbar />
+                    <header><h1>California state history</h1></header>
+                </div>
+                <div style={{height: "85vh"}}>
+                    <ResponsiveLine 
+                        data={this.state.graphData}
+                        enablePoints={false}
+                        xScale={{
+                            type: 'time',
+                            format: '%Y-%m-%d',
+                            useUTC: true,
+                        }}
+                        xFormat="time:%Y-%m-%d"
+                        yScale={{
+                            type: 'linear',
+                        }}
+                        yFormat=">,.2f"
+                        margin={{
+                            top: 0,
+                            bottom: 40,
+                            left: 60,
+                            right: 50
+                        }}
+                        axisBottom={{
+                            orient: 'bottom',
+                            format: '%Y-%m-%d',
+                            tickSize: 5,
+                            tickPadding: 5,
+                            legend: 'Time',
+                            legendPosition: 'middle',
+                            legendOffset: 30,
+                        }}
+                    />
+                </div>
+            </div>
         )
     }
 }
